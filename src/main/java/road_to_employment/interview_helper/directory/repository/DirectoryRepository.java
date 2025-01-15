2d5f80b6e3b6711da564c7fa18bb7e707b2efb3b
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2025-01-15 11:45:36 +0900

    feat: [한재혁] 사용자가 가지고 있는 디렉토리 리스트를 반환할 수 있습니다. [IHB-13]

--- src/main/java/road_to_employment/interview_helper/directory/repository/DirectoryRepository.java
+++ src/main/java/road_to_employment/interview_helper/directory/repository/DirectoryRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface DirectoryRepository extends JpaRepository<Directory, Long> {
     boolean existsByLibraryAndName(Library library, String name);
+    List<Directory> findByLibraryIn(Library library);
 }
--- src/main/java/road_to_employment/interview_helper/directory/service/DirectoryService.java
+++ src/main/java/road_to_employment/interview_helper/directory/service/DirectoryService.java
@@ -3,8 +3,10 @@ package road_to_employment.interview_helper.directory.service;
 
 import road_to_employment.interview_helper.directory.entity.Directory;
 
+import java.util.List;
 
 public interface DirectoryService {
     boolean checkDirectoryDuplicate(String name, Library library);
     Directory createDirectory(String name, Library library);
+    List<Directory> listDirectory(Library library);
 }
