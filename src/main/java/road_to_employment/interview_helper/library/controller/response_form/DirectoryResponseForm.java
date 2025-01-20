cc5066f8613f3433431233cc9e0468441b4c4d57
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2025-01-20 14:25:30 +0900

    add: [한재혁] response, responseform에 createDate 추가 [IHB-11,12,13]

--- src/main/java/road_to_employment/interview_helper/library/controller/response_form/DirectoryResponseForm.java
+++ src/main/java/road_to_employment/interview_helper/library/controller/response_form/DirectoryResponseForm.java
@@ -14,12 +14,15 @@ public class DirectoryResponseForm {
     private final Long id;
     private final String name;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private final LocalDateTime createDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private final LocalDateTime updateDate;
 
     public static DirectoryResponseForm from(DirectoryResponse directoryResponse) {
         return new DirectoryResponseForm(
                 directoryResponse.getId(),
                 directoryResponse.getName(),
+                directoryResponse.getCreateDate(),
                 directoryResponse.getUpdateDate());
     }
 }
